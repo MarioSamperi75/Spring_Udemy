fbbc87ef6e571bd33ca12014da4629bb78f4e111
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-11 10:41:36 +0100

    Pointcut Expression: matching all methods starting with add -  wildcard - Demo

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,10 +13,12 @@ public class MyDemoLoggingAspect {
 	//let's start with an @Before advice
 	
 	// Pointcut syntax: execution( verifier? returnType class.? methodName(params)  throws?) 
-	// macthing addAccount method in any class : @Before("execution(public void addAccount())")
 	
-	//matching addAccount method just in ONE class
-	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+	// macthing addAccount method in any class : @Before("execution(public void addAccount())")
+	// matching addAccount method just in ONE class : @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+	
+	// matching all methods starting with add - * as wildcard :
+	@Before("execution(public void add*())")
 		public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>> Executing @Before advice on AddAccount()");
 	}
