c76a32df3e321f83348401c0f42dafb7127d2ffe
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-11 11:08:14 +0100

    Pointcut Expression: working with parameters - summary

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -16,11 +16,20 @@ public class MyDemoLoggingAspect {
 	
 	// macthing addAccount method in any class : @Before("execution(public void addAccount())")
 	// matching addAccount method just in ONE class : @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+	// matching all methods starting with add - * as wildcard :@Before("execution(public void add*())")
+	// matching all methods starting with add and with any return type: @Before("execution(public * add*())")
+	// () no parameters 
+	// (*) one argument of any type 
+	// (..) 0 or more arguments of any type
+	// matching with an argument of a specific type (use qualified name):
+			// @Before("execution(public * addAccount(com.luv2code.aopdemo.dao.AccountDAO))")
+	// matching with all methods in a specific package:
+			// @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	
-	// matching all methods starting with add - * as wildcard :
+	
 	@Before("execution(public void add*())")
 		public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>> Executing @Before advice on AddAccount()");
 	}
-
+  
 }
