64f0aef7c9997fab4d9bf30fce5d5e10f23f09a1
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-12 10:55:51 +0100

    Create and apply a combo pointcut declaration - demo

---
+++ spring-demo-pointcut-declaration-combo/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,44 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	// Create pointcut declaration
+	// we will reuse just the name of the method!
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	
+	// create pointcut include package ...exclude  getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {};
+	
+	
+	
+	//Apply the  combo pointcut declaration to both advices
+	@Before("forDaoPackageNoGetterSetter()")
+		public void beforeAddAccountAdvice() {
+		System.out.println("\n=====>> Executing @Before advice on AddAccount()");
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+	System.out.println("\n=====>> Performing API Analytics");
+}
+  
+}
