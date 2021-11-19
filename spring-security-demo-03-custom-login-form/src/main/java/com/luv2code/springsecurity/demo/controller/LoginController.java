07470343e38a500dd10962d3bfcf78a3c85ed2ba
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-19 22:28:06 +0100

    Spring security: Create the custom Login page

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.luv2code.springsecurity.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LoginController {
 	
 	@GetMapping("/showMyLoginPage")
