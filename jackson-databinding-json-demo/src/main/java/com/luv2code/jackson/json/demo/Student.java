e7c8034d5bcc37271bb1fd87ec3b05ce247b1dd5
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-24 11:22:31 +0100

    Spring REST -JSON Jackson Demo - from Json to object

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -1,5 +1,47 @@
 package com.luv2code.jackson.json.demo;
 
 public class Student {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private boolean active;
+	
+	public Student () {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	};
+	
+	
+	
 
 }
