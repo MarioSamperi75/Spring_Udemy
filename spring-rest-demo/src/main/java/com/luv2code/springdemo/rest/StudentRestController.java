f4a5a894132526ce71f7a0911cdf3d8f51411d2f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-26 20:49:32 +0100

    Spring REST - Retrieve POJO as JSON - refactoring Student

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.catalina.valves.LoadBalancerDrainingValve;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +16,22 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define endpoint for /student -returns list of students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	List<Student> theStudents = new ArrayList<>();
+	
+	@PostConstruct
+	public void LoadData() {
 		
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Mario", "Samperi"));
 		theStudents.add(new Student("Ellen", "Bredefeldt"));
 		theStudents.add(new Student("Alexander", "Samperi-Bredefeldt"));
 		
-		
-		
+	}
+	
+	// define endpoint for /student -returns list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return theStudents;
 	}
 
