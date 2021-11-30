159ee8accc8a4e2048c35b9ffc7bec9dde2a7c66
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-11-30 11:34:27 +0100

    Spring REST CRUD - Getting a single customer

--- spring-crm-rest-demo/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -2,8 +2,10 @@ package com.luv2code.springdemo.rest;
 
 import java.util.List;
 
+import org.hibernate.hql.spi.id.TableBasedDeleteHandlerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +20,20 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerService customerService; 
 	
-	// add mapping for get customer
+	// add mapping to get all the customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers () {
 		
 		return customerService.getCustomers();
+	} 
+	
+	// add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer (@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
 	} 
 
 }
