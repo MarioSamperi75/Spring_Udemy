5ebd0c3130d9621e2c22560168e2ba6c690248e6
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-12-02 20:34:35 +0100

    fixed folders, artifact name och mvmw

--- dev-springboot/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ dev-springboot/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.demo.myspringbootapp;
+package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyspringbootappApplication {
+public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootappApplication.class, args);
+		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 }
--- dev-springboot/cruddemo/src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
+++ dev-springboot/cruddemo/src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.demo.myspringbootapp;
+package com.luv2code.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyspringbootappApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
