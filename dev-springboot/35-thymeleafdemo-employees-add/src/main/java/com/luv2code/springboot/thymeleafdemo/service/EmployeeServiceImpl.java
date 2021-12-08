5403c33eea5aeba39a3b46b397a2ba73e48eff4a
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-12-08 10:22:26 +0100

    Spring Boot - Thymeleaf - CRUD db - add Employee - order by last nme

--- dev-springboot/35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ dev-springboot/35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -26,7 +26,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public List<Employee> findAll() {
-		return employeeRepository.findAll();
+		return employeeRepository.findAllByOrderByLastNameAsc();
 	}
 
 	@Override
